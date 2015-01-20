308c386192373e531a616f2e758c77d7ea88df7a
Deszip <raf.rafiki@gmail.com>
2015-01-20 12:11:17 +0200

    master - pods added to repo

---
+++ Example/Pods/Target Support Files/Pods-APCDController_Tests/Pods-APCDController_Tests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// APCDController
+#define COCOAPODS_POD_AVAILABLE_APCDController
+#define COCOAPODS_VERSION_MAJOR_APCDController 0
+#define COCOAPODS_VERSION_MINOR_APCDController 1
+#define COCOAPODS_VERSION_PATCH_APCDController 4
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 3
+#define COCOAPODS_VERSION_PATCH_Expecta 1
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 3
+#define COCOAPODS_VERSION_MINOR_OCMock 1
+#define COCOAPODS_VERSION_PATCH_OCMock 2
+
